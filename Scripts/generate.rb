30fffe87872342c6486ac3b179266d4922b525f7
Jonas Bengtsson <jonas.b@gmail.com>
2013-09-24 23:01:09 +0200

    Refaktorera generate.rb

--- Scripts/generate.rb
+++ Scripts/generate.rb
@@ -8,73 +8,97 @@ SCRIPTS_DIR = File.dirname(__FILE__)
 OUTPUT_DIR = File.expand_path(File.join(SCRIPTS_DIR, '..', 'HTML'))
 TEXT_DIR = File.expand_path(File.join(SCRIPTS_DIR, '..', 'Text'))
 
-# helpers
-def die(msg)
-  warn msg
-  exit -1
-end
-
-# parse options
-options = {}
-OptionParser.new do |opt|
-  opt.on('-f', '--force', 'delete output directory if already exists') do
-    options[:force] = true
+class Generator
+  def main
+    parse_options
+    prepare_output_dir
+    markdown_files = get_markdown_files
+    generate_index_html markdown_files
   end
-  opt.on('-n', '--dryrun', 'dryrun') do
-    options[:dryrun] = true
+
+  def die(msg)
+    warn msg
+    exit -1
   end
-  opt.on('-v', '--verbose', 'verbose') do
-    options[:verbose] = true
+
+  def parse_options
+    @options = {}
+    OptionParser.new do |opt|
+      opt.on('-f', '--force', 'delete output directory if already exists') do
+        @options[:force] = true
+      end
+      opt.on('-n', '--dryrun', 'dryrun') do
+        @options[:dryrun] = true
+      end
+      opt.on('-v', '--verbose', 'verbose') do
+        @options[:verbose] = true
+      end
+    end.parse!
+
+    @fileutils_options = {}
+    @fileutils_options[:verbose] = @options[:verbose]
+    @fileutils_options[:noop] = @options[:dryrun]
+    @fileutils_options[:force] = @options[:force]
   end
-end.parse!
 
-fileutils_options = {}
-fileutils_options[:verbose] = options[:verbose]
-fileutils_options[:noop] = options[:dryrun]
-fileutils_options[:force] = options[:force]
-
-# prepare output dir
-if File.exists? OUTPUT_DIR then
-  if options[:force] then
-    # remove all files in dir
-    FileUtils.rm_r(File.join(OUTPUT_DIR, '.'), fileutils_options)
-  else
-    die "Directory #{OUTPUT_DIR} already exists"
+  def prepare_output_dir
+    if File.exists? OUTPUT_DIR then
+      if @options[:force] then
+        # remove all files in dir
+        FileUtils.rm_r(File.join(OUTPUT_DIR, '.'), @fileutils_options)
+      else
+        die "Directory #{OUTPUT_DIR} already exists"
+      end
+    else
+      FileUtils.mkdir(OUTPUT_DIR, @fileutils_options)
+    end
   end
-else
-  FileUtils.mkdir(OUTPUT_DIR, fileutils_options)
-end
 
-# get texts
-markdown_files = Dir[File.join(TEXT_DIR, '*.md')]
-markdown_files.empty? && die("Could not find any files in #{TEXTS_DIR}")
+  def get_markdown_files
+    markdown_files = Dir[File.join(TEXT_DIR, '*.md')]
+    markdown_files.empty? && die("Could not find any files in #{TEXTS_DIR}")
+    markdown_files
+  end
 
-# setup index.html
-index_filename = File.join(OUTPUT_DIR, 'index.html')
-options[:verbose] && warn("Generating #{index_filename}")
-index_html = options[:dryrun] ? STDERR : File.open(index_filename, 'w')
-index_html.write <<eos
-<!DOCTYPE html>
-<html lang="sv">
-<head>
-<meta charset="utf-8">
-<title>HaxeSpel</title>
-<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-</head>
-<body>
-<div class="container">
+  def generate_index_html(markdown_files)
+    index_html = setup_index_html
+    generate_chapters(index_html, markdown_files)
+    finish_index_html(index_html)
+  end
+
+  def setup_index_html
+    index_filename = File.join(OUTPUT_DIR, 'index.html')
+    @options[:verbose] && warn("Generating #{index_filename}")
+    index_html = @options[:dryrun] ? STDERR : File.open(index_filename, 'w')
+    index_html.write <<eos
+    <!DOCTYPE html>
+    <html lang="sv">
+    <head>
+    <meta charset="utf-8">
+    <title>HaxeSpel</title>
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+    </head>
+    <body>
+    <div class="container">
 eos
+    index_html
+  end
 
-# parse markdown
-markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
-markdown_files.each do |markdown_file|
-  index_html.write(markdown.render(IO.read(markdown_file)))
+  def generate_chapters(index_html, markdown_files)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
+    markdown_files.each do |markdown_file|
+      index_html.write(markdown.render(IO.read(markdown_file)))
+    end
+  end
+
+  def finish_index_html(index_html)
+    index_html.write <<eos
+    </div>
+    </body>
+    </html>
+eos
+    !@options[:dryrun] || index_html.close
+  end
 end
 
-# finish index_html
-index_html.write <<eos
-</div>
-</body>
-</html>
-eos
-!options[:dryrun] || index_html.close
+Generator.new.main()
